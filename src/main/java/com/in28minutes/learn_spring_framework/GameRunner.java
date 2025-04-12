70df3b81f433afc9b895c4dd378c2fe62534d9c9
achoukse <abhilashchouksey@gmail.com>
2025-04-11 23:37:16 -0400

    test

---
+++ src/main/java/com/in28minutes/learn_spring_framework/GameRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_spring_framework;
+
+public class GameRunner {
+    MarioGame game;
+
+    public GameRunner(MarioGame game) {
+        this.game =game;
+    }
+
+    public void run() {
+        System.out.println("Running Game:"+ game);
+    }
+}
