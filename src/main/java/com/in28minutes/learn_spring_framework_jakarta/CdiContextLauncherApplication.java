08a4568c6c546b4cc8071bc441c7a9d5aa7002a6
achoukse <abhilashchouksey@gmail.com>
2025-04-28 14:09:35 -0400

    test

---
+++ src/main/java/com/in28minutes/learn_spring_framework_jakarta/CdiContextLauncherApplication.java
@@ -0,0 +1,43 @@
+package com.in28minutes.learn_spring_framework_jakarta;
+
+
+import com.in28minutes.learn_spring_frameworkExaple1.BusinessCalculationService;
+import com.in28minutes.learn_spring_frameworkExaple1.RealWorldSpringContextLauncherApplication;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+//@Component
+@Named
+class BusinessService{
+    private DataService dataService;
+
+    public DataService getDataService() {
+        return dataService;
+    }
+//@Autowired
+    @Inject
+    public void setDataService(DataService dataService) {
+        this.dataService = dataService;
+    }
+}
+@Component
+class DataService{
+
+}
+@Configuration
+@ComponentScan
+public class CdiContextLauncherApplication {
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(CdiContextLauncherApplication.class)){
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean(BusinessService.class));
+
+        }
+    }
+}
